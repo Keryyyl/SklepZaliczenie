cff7cf1066ccc39273bef36433dd657c95162c8e
Keryyyl <karol.idzkowski06@gmail.com>
2025-03-25 20:46:56 +0100

    Adds routing to navbar

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { Link } from "react-router-dom";
 
 function Header() {
     const [darkMode, setDarkMode] = useState(
@@ -25,11 +27,11 @@ function Header() {
     return (
         <div className="navbar shadow-sm fixed top-0 w-full dark:bg-gray-800 bg-gray-200 transition duration-2500">
             <div className="flex w-full justify-between p-4">
-                <a className="text-3xl ml-5 dark:text-white btn btn-ghost hover:text-base-content text-black transition duration-500">Kursiory</a>
+                <Link to="/" className="text-3xl ml-5 dark:text-white btn btn-ghost hover:text-base-content text-black transition duration-500">Kursiory</Link>
                 <div className="flex items-center">
-                    <button className="text-2xl mr-10 btn btn-ghost hover:text-base-content text-black dark:text-white transition duration-500">
+                    <Link to="koszyk" className="text-2xl mr-10 btn btn-ghost hover:text-base-content text-black dark:text-white transition duration-500">
                         Koszyk
-                    </button>
+                    </Link>
                     <button id="lightswitch" className="text-2xl mr-10 btn btn-square btn-ghost transition duration-500" onClick={toggleDarkMode}>
                         <span className="material-icons">
                             {darkMode ? "dark_mode" : "light_mode"}
