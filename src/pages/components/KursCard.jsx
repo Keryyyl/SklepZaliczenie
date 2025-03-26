1e310a513ee90290c96b3e3a958bfb1999d69c34
Keryyyl <karol.idzkowski06@gmail.com>
2025-03-26 17:45:22 +0100

    Fixes style for Kurscard

--- src/pages/components/KursCard.jsx
+++ src/pages/components/KursCard.jsx
@@ -1,15 +1,15 @@
 
 function KursCard(props) {
-
     return (
       <>
-        <div className="card dark:bg-gray-800 bg-slate-300 border-3 border-gray-800 dark:border-slate-300 text-accent w-96 transition duration-2500 ">
+
+        <div className="card dark:bg-gray-800 bg-slate-300 shadow-xl shadow-gray-700 dark:shadow-gray-900 rounded-lg text-accent w-96 transition duration-2500 ">
           <div className="card-body">
             <h2 className="card-title text-3xl">{props.title}</h2>
             <p className="text-xl">{props.description}</p>
             <div className="card-actions align-middle justify-between">
               <label className="text-2xl mt-1">{props.price} zł</label>
-              <button className="btn btn-ghost border-2 border-black dark:border-slate-300 transition duration-2500">Dodaj do koszyka</button>
+              <button className="btn btn-ghost border-2 border-accent transition duration-2500">Dodaj do koszyka</button>
             </div>
           </div>
         </div>
