580e8cec7ceb3298537c78f3ef05ff8f41fc2743
Keryyyl <karol.idzkowski06@gmail.com>
2025-03-26 13:05:10 +0100

    Changes header styling

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -26,12 +26,12 @@ function Header() {
     return (
         <div className="navbar shadow-sm fixed top-0 w-full dark:bg-gray-800 bg-gray-200 transition duration-2500">
             <div className="flex w-full justify-between p-4">
-                <Link to="/" className="text-3xl ml-5 dark:text-white btn btn-ghost hover:text-base-content text-black transition duration-500">Kursiory</Link>
+                <Link to="/" className="text-3xl ml-5 btn btn-ghost hover:text-text-accent  text-accent transition duration-800">Kursiory</Link>
                 <div className="flex items-center">
-                    <Link to="/koszyk" className="text-2xl mr-10 btn btn-ghost hover:text-base-content text-black dark:text-white transition duration-500">
+                    <Link to="/koszyk" className="text-2xl mr-10 btn btn-ghost hover:text-accent text-accent transition duration-800">
                         Koszyk
                     </Link>
-                    <button id="lightswitch" className="text-2xl mr-10 btn btn-square btn-ghost transition duration-500" onClick={toggleDarkMode}>
+                    <button id="lightswitch" className="text-2xl mr-10 btn btn-square btn-ghost transition duration-800" onClick={toggleDarkMode}>
                         <span className="material-icons">
                             {darkMode ? "dark_mode" : "light_mode"}
                         </span>
